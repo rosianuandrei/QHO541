431637fe8d2052972247ba84db1413d5f82ba0b9
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 17:19:49 +0300

    resolve bugs

--- api/config.js
+++ api/config.js
@@ -3,7 +3,7 @@ const config = {
     connection: {
         host : 'ec2-34-225-159-178.compute-1.amazonaws.com',
         user : 'ojewmrompuemji',
-        password : 'c2eff6c3a73441a1e4c5201cf1c8259b7e330c25fa8a1c2a7dddfdd7134ec10c',
+        password : '12345',
         database : 'd80poge5f70ke8'
     }
 };
