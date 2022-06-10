cb13485b79a75388b3a60cae841f447e6e43c9d5
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 13:30:10 +0300

    getAll function

--- api/models/application.js
+++ api/models/application.js
@@ -2,8 +2,13 @@ const db = require('../helpers/database');
 
 const addApplication = async (application) => {
     let result = await db.insert(application).into('applications').returning('*').catch((err) => err);
+    return result;
+}
+
+const getAll = async () => {
+    let result = await db.select('*').from('applications');
     console.log(result);
     return result;
 }
 
-module.exports = {addApplication};
+module.exports = {addApplication, getAll};
--- api/routes/applications.js
+++ api/routes/applications.js
@@ -14,6 +14,18 @@ const createApplication = async (req, res) => {
     }
 }
 
+const getAllApplications = async (req, res) => {
+    let result = await applicationsModel.getAll().catch((err) => console.log(err));
+    if (Array.isArray(result) && result.length) {
+        return res.status(200).json(result);
+    } else if (Array.isArray(result) && result.length === 0) {
+        return res.status(404).json('No applications found');
+    } else {
+        return res.status(500).json('An error occurred when trying to get applications');
+    }
+}
+
 router.post('/', createApplication);
+router.get('/', getAllApplications);
 
 module.exports = router;
