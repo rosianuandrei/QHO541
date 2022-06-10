01c8cb4d76bf6c111fa2dba00b21507e8759f89c
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 15:28:24 +0300

    user state, login and logout

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -13,6 +13,7 @@ import Application from './components/Application';
 import Register from './components/Register';
 import Login from './components/Login';
 import CreateApplication from './components/CreateApplication';
+import UserContext from './contexts/user';
 
 const { Header, Content, Footer } = Layout;
 
@@ -20,24 +21,26 @@ function App() {
   return (
     <>
       <Layout className="layout">
-        <Router>
-          <Header>
-            <Nav />
-          </Header>
+        <UserContext.Provider value={context}>
+          <Router>
+            <Header>
+              <Nav />
+            </Header>
 
-          <Content>
-            <Switch>
-              <Route path='/account' children={<Account />} />
-              <Route path='/application/:id' children={<Application />} />
-              <Route path='/register' children={<Register />} />
-              <Route path='/login' children={<Login />} />
-              <Route path='/create' children={<CreateApplication />} />
-              <Route path='/' children={<Home />} />
-            </Switch>
-          </Content>
+            <Content>
+              <Switch>
+                <Route path='/account' children={<Account />} />
+                <Route path='/application/:id' children={<Application />} />
+                <Route path='/register' children={<Register />} />
+                <Route path='/login' children={<Login />} />
+                <Route path='/create' children={<CreateApplication />} />
+                <Route path='/' children={<Home />} />
+              </Switch>
+            </Content>
 
-          <Footer style={{ textAlign: 'center' }}>Created for QHO541</Footer>
-        </Router>
+            <Footer style={{ textAlign: 'center' }}>Created for Web API Development 6003CEM</Footer>
+          </Router>
+        </UserContext.Provider>
       </Layout>
     </>
   );
