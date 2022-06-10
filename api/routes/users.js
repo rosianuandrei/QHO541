dfea3cc305cb55cf9a63cc74bb40901a383536ee
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 13:15:18 +0300

    updateUser and error handling

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,33 +10,49 @@ router.use(function timeLog (req, res, next) {
 })
 
 const createUser = async (req, res) => {
-    let result = await usersModel.addUser(req.body).catch(err => res.status(400).json('Error registering the user'));
+    let result = await usersModel.addUser(req.body).catch((err) => console.log(err));
     if (result) {
         return res.status(200).json('User registered sucessfully');
     }
 }
 
 const getAll = async (req, res) => {
-    let result = await usersModel.getAll().catch(err => res.status(400).json('Error getting list of users'));
-    if (result.length) {
+    let result = await usersModel.getAll().catch((err) => console.log(err));
+    if (Array.isArray(result) && result.length) {
         return res.status(200).json(result);
+    } else if (Array.isArray(result) && result.length === 0) {
+        return res.status(404).json('No user found');
     } else {
-        return res.status(404).json('No users found');
+        return res.status(500).json('Error was found');
     }
 }
 
 const getById = async (req, res) => {
     let { id } = req.params;
-    let result = await usersModel.getById(id).catch(err => res.status(400).json('Error getting a user'));
-    if (result.length) {
+    let result = await usersModel.getById(id).catch((err) => console.log(err));
+    console.log(result);
+    if (Array.isArray(result) && result.length) {
         return res.status(200).json(result);
-    } else {
+    } else if (Array.isArray(result) && result.length === 0) {
         return res.status(404).json('No user found');
+    } else {
+        return res.status(500).json('Error was found');
+    }
+}
+
+const updateUser = async (req, res) => {
+    let { id } = req.params;
+    let result = await usersModel.updateUser(req.body, id).catch((err) => console.log(err));
+    if (result === 1) {
+        return res.status(200).json('User updated succesfully');
+    } else {
+        return res.status(400).json('Error updating user');
     }
 }
 
 router.get('/:id', getById);
 router.post('/', createUser);
+router.put('/:id', updateUser);
 router.get('/', getAll);
 
 module.exports = router; 
