a03fde7fbbadd5e7296d1f2de2b3aa490a449201
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 17:30:05 +0300

    resolve bugs 6

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 const config = {
     client: 'pg',
     connection: {
-        host : 'ec2-34-225-159-178.compute-1.amazonaws.com',
+        host : 'localhost',
         user : 'ojewmrompuemji',
         password : '1234567',
         database : 'postgresql-curly-06855'
