6531fff12e61877fcabec16f1fedb8d6b5a66669
Andrei Rosianu <andrei_rosianu@hotmail.co.uk>
2022-06-10 18:22:49 +0300

    resolve bugs 15

--- api/config.js
+++ api/config.js
@@ -1,11 +1,12 @@
 const config = {
     client: 'pg',
-    connection: {
+    /*connection: {
         host : 'mysterious-wildwood-43310.herokuapp.com',
         user : 'ojewmrompuemji',
         password : '1234567',
         database : 'postgresql-curly-06855',
-    },
+    },*/
+    connection: 'postgres://ojewmrompuemji:c2eff6c3a73441a1e4c5201cf1c8259b7e330c25fa8a1c2a7dddfdd7134ec10c@ec2-34-225-159-178.compute-1.amazonaws.com:5432/d80poge5f70ke8',
     pool: { "min": 0,
     "max": 10000,
     "createTimeoutMillis": 3000,
